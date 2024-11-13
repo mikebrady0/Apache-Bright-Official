07dbac1d5072d3507f36a3f5766eea10f779080b
mikebrady0 <147462552+mikebrady0@users.noreply.github.com>
2024-11-13 17:10:40 -0500

    set gh-pages

---
+++ apache-bright-official/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+  <div className="background">
+    <div className="content">
+      <h1 className="text">Under Construction</h1>
+      <h3 className="text">Apache Bright Detailing page is currently under construction...</h3>
+      <p className="text">In the meantime, feel free to follow us on instagram <a className="text-link" href="https://www.instagram.com/apachebrightllc/">@apachebrightllc</a></p>
+  </div>
+</div>
+  );
+}
+
+export default App;
